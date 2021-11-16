dd358ffb4b149c78bdb9f9b7507e4f2257520c7f
H0rologium <33498180+H0rologium@users.noreply.github.com>
2021-11-16 16:58:58 -0600

    Update quarry.lua

--- Mining/quarry.lua
+++ Mining/quarry.lua
@@ -1,25 +1,31 @@
 local tArgs = { ... }
 if #tArgs ~= 2 then
-	print( "Usage: quarry <diameter> <startingylevel>" )
+	print( "Usage: quarry <diameter> <depth>" )
 	return
 end
 
+local diameter = tArgs[1]
+local depth = tArgs[2]
+--Coordinates array where 0 is x, 1 is y and 2 is z
+local coordinates = [0,0,0]
 
+preInit()
 
+local function preInit()
+	if fs.exists("coords.txt") then
+	
+	else 
+		local f = fs.open("coords.txt", "w")
+	
+		f.close()
+	end
+	
+end
 
+local function move(direction)
+	print('bluh')
 
-
-
-
-
-
-
-
-
-
-
-
-
+end
 
 
 
